b48e988e7a5c59a42ae0df812231102b3834b155
ErickBGomez <90425287+ErickBGomez@users.noreply.github.com>
2025-02-20 15:47:35 -0600

    Add comments to improve readability

--- index.js
+++ index.js
@@ -4,10 +4,13 @@ const { loadHtml } = require("./modules/loadHtml");
 const server = http.createServer();
 
 server.on("request", (req, res) => {
+  // Ignore favicon requests
   if (req.url === "/favicon.ico") return;
 
+  // Find html file based in req.url value, and return their respective status code and file content
   const { status, content } = loadHtml(req.url);
 
+  // Send content response
   res.writeHead(status, { "Content-Type": "text/html" });
   res.write(content);
   res.end();
