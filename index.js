f34a2a387d6795d807e7999701c93757971d3940
ErickBGomez <90425287+ErickBGomez@users.noreply.github.com>
2025-02-20 17:58:26 -0600

    Handle index GET request

--- index.js
+++ index.js
@@ -1,28 +1,48 @@
-const http = require("http");
-const process = require("process");
+// const http = require("http");
+// const process = require("process");
 const loadHtml = require("./modules/loadHtml");
 
-const server = http.createServer();
+// const server = http.createServer();
 
-server.on("request", (req, res) => {
-  // Ignore favicon requests
-  if (req.url === "/favicon.ico") return;
+// server.on("request", (req, res) => {
+//   // Ignore favicon requests
+//   if (req.url === "/favicon.ico") return;
 
-  // Find html file based in req.url value, and return their respective status code and file content
-  const { status, content } = loadHtml(req.url);
+//   // Find html file based in req.url value, and return their respective status code and file content
+//   const { status, content } = loadHtml(req.url);
 
-  // Send content response
-  res.writeHead(status, { "Content-Type": "text/html" });
-  res.write(content);
-  res.end();
+//   // Send content response
+//   res.writeHead(status, { "Content-Type": "text/html" });
+//   res.write(content);
+//   res.end();
+// });
+
+// server.listen(8080, () => {
+//   console.log("Server initialized!");
+
+//   // Handle exit signal
+//   process.on("SIGINT", () => {
+//     console.log("\nServer stopped!");
+//     process.exit();
+//   });
+// });
+
+const express = require("express");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.get("/", (req, res, next) => {
+  const options = {
+    root: __dirname,
+  };
+
+  res.sendFile("./html/index.html", options);
 });
 
-server.listen(8080, () => {
+app.listen(PORT, () => {
   console.log("Server initialized!");
-
-  // Handle exit signal
-  process.on("SIGINT", () => {
-    console.log("\nServer stopped!");
-    process.exit();
-  });
 });
