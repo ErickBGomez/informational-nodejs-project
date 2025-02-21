57563cbd8f07aca60fd361e80b2be4d196169974
ErickBGomez <90425287+ErickBGomez@users.noreply.github.com>
2025-02-20 18:33:04 -0600

    Cleaning code

--- index.js
+++ index.js
@@ -1,37 +1,8 @@
-// const http = require("http");
-// const process = require("process");
-const loadHtml = require("./modules/loadHtml");
-
-// const server = http.createServer();
-
-// server.on("request", (req, res) => {
-//   // Ignore favicon requests
-//   if (req.url === "/favicon.ico") return;
-
-//   // Find html file based in req.url value, and return their respective status code and file content
-//   const { status, content } = loadHtml(req.url);
-
-//   // Send content response
-//   res.writeHead(status, { "Content-Type": "text/html" });
-//   res.write(content);
-//   res.end();
-// });
-
-// server.listen(8080, () => {
-//   console.log("Server initialized!");
-
-//   // Handle exit signal
-//   process.on("SIGINT", () => {
-//     console.log("\nServer stopped!");
-//     process.exit();
-//   });
-// });
-
 const path = require("path");
 const express = require("express");
 const dotenv = require("dotenv");
-const notFoundMiddleware = require("./middleware/notFound");
-const serverErrorMiddleware = require("./middleware/serverError");
+const notFoundMiddleware = require("./middlewares/notFound");
+const serverErrorMiddleware = require("./middlewares/serverError");
 
 dotenv.config();
 
@@ -57,7 +28,6 @@ app.get(
   "/:name",
   (req, res, next) => {
     const { name } = req.params;
-    console.log(name);
 
     res.sendFile(`${name}.html`, fileOptions, (e) => {
       if (e) next();
